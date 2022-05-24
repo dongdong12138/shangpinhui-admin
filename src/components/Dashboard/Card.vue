e238f67e2902f7a477d88ec381915995cf1d8e6b
BarryDong <286084845@qq.com>
2022-05-24 20:48:35 +0800

    首页-Card静态组件

---
+++ src/components/Dashboard/Card.vue
@@ -0,0 +1,43 @@
+<template>
+  <section>
+    <header>
+      <span>{{ title }}</span>
+      <i class="el-icon-info" />
+    </header>
+    <section class="price">{{ count }}</section>
+    <section class="charts">
+      <slot name="charts" />
+    </section>
+    <footer>
+      <slot name="footer" />
+    </footer>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  // eslint-disable-next-line vue/require-prop-types
+  props: ['title', 'count']
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  color: #d9d9d9;
+}
+.price {
+  font-size: 30px;
+  padding: 10px 0;
+}
+.charts {
+  height: 40px;
+  line-height: 40px;
+}
+footer {
+  border-top: 1px solid #eee;
+  padding-top: 10px;
+}
+</style>
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,30 +1,90 @@
 <template>
-  <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+  <div>
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <el-card>
+          <Card title="总销售额" count="￥12345">
+            <template slot="charts">
+              <div style="font-size: 15px">
+                <span>周同比 56.67%</span><i class="icon up arrow" style="top: -3px" />
+                <span>日同比 16.67%</span><i class="icon down arrow" style="top: 3px" />
+              </div>
+            </template>
+            <template slot="footer">
+              <span>日销售额 ￥2367</span>
+            </template>
+          </Card>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card>
+          <Card title="访问量" count="5482">
+            <template slot="charts">
+              <div>222</div>
+            </template>
+            <template slot="footer">
+              <span>日访问量 1234</span>
+            </template>
+          </Card>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card>
+          <Card title="支付笔数" count="562">
+            <template slot="charts">
+              <div>333</div>
+            </template>
+            <template slot="footer">
+              <span>转化率 65%</span>
+            </template>
+          </Card>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card>
+          <Card title="运营活动效果" count="59%">
+            <template slot="charts">
+              <div>444</div>
+            </template>
+            <template slot="footer">
+              <div style="font-size: 15px">
+                <span>周同比 56.67%</span><i class="icon up arrow" style="top: -3px" />
+                <span>日同比 16.67%</span><i class="icon down arrow" style="top: 3px" />
+              </div>
+            </template>
+          </Card>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import Card from '@/components/Dashboard/Card'
 
 export default {
   name: 'Dashboard',
+  components: { Card },
   computed: {
-    ...mapGetters([
-      'name'
-    ])
+    ...mapGetters(['name'])
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.dashboard {
-  &-container {
-    margin: 30px;
-  }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
-  }
+i.arrow {
+  display: inline-block;
+  width: 0;
+  height: 0;
+  border: 5px solid transparent;
+  &.up { border-bottom-color: red; }
+  &.down { border-top-color: green; }
+}
+
+.icon {
+  margin-right: 8px;
+  margin-left: 2px;
+  position: relative;
 }
 </style>
