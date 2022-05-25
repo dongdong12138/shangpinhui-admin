f5e5e49e69de9abbd82e72e65a41414c2e93cbcd
BarryDong <286084845@qq.com>
2022-05-25 10:58:37 +0800

    Sale.vue 页面tab切换 & 日期选择

--- src/components/Dashboard/Sale.vue
+++ src/components/Dashboard/Sale.vue
@@ -7,13 +7,15 @@
         <el-tab-pane label="访问量" name="visitCount" />
       </el-tabs>
       <div class="date-container">
-        <span>今日</span><span>本周</span><span>本月</span><span>本年</span>
+        <span @click="setDay">今日</span><span @click="setWeek">本周</span>
+        <span @click="setMonth">本月</span><span @click="setYear">本年</span>
         <el-date-picker
-          v-model="value1"
+          v-model="dateArr"
           style="width: 250px; margin-left: 20px"
           size="mini"
           type="daterange"
           range-separator="-"
+          value-format="yyyy-MM-dd"
           start-placeholder="开始日期"
           end-placeholder="结束日期"
         />
@@ -25,7 +27,7 @@
             <div ref="charts" class="charts" />
           </el-col>
           <el-col :span="6">
-            <h3 class="saleRank-title">门店销售额排名</h3>
+            <h3 class="saleRank-title">门店{{ title }}排名</h3>
             <ol class="saleRank-list">
               <li v-for="item in 7" :key="item">
                 <i :class="{ top3: item <= 3 }">{{ item }}</i><div>肯德基<span>124578</span></div>
@@ -41,18 +43,34 @@
 
 <script>
 import * as echarts from 'echarts'
+import dayjs from 'dayjs'
 
 export default {
   name: 'Sale',
   data() {
     return {
       activeName: 'saleCount',
-      value1: ''
+      dateArr: [],
+      myChart: null
+    }
+  },
+  computed: {
+    title() {
+      return this.activeName === 'saleCount' ? '销售额' : '访问量'
+    }
+  },
+  watch: {
+    title(newValue) {
+      this.myChart.setOption({
+        title: {
+          text: `${newValue}趋势`
+        }
+      })
     }
   },
   mounted() {
-    const myChart = echarts.init(this.$refs.charts)
-    myChart.setOption({
+    this.myChart = echarts.init(this.$refs.charts)
+    this.myChart.setOption({
       title: {
         text: '销售额趋势'
       },
@@ -95,6 +113,26 @@ export default {
   methods: {
     handleClick(tab, event) {
       console.log(tab, event)
+    },
+
+    setDay() {
+      const day = dayjs().format('YYYY-MM-DD')
+      this.dateArr = [day, day]
+    },
+    setWeek() {
+      const monday = dayjs().day(1).format('YYYY-MM-DD')
+      const sunday = dayjs().day(7).format('YYYY-MM-DD')
+      this.dateArr = [monday, sunday]
+    },
+    setMonth() {
+      const start = dayjs().startOf('month').format('YYYY-MM-DD')
+      const end = dayjs().endOf('month').format('YYYY-MM-DD')
+      this.dateArr = [start, end]
+    },
+    setYear() {
+      const start = dayjs().startOf('year').format('YYYY-MM-DD')
+      const end = dayjs().endOf('year').format('YYYY-MM-DD')
+      this.dateArr = [start, end]
     }
   }
 }
